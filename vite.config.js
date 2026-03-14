c2d99001c9bcf0490aaa7a03a134c8a56bec28e6
ItsTiazzz <163735662+ItsTiazzz@users.noreply.github.com>
2026-03-14 20:52:53 +0100

    Yes maybe include those

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import { resolve } from 'path'
 export default {
     root: resolve(__dirname, 'src'),
     build: {
-        outDir: '../dist'
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                discord: resolve(__dirname, 'src/discord.html'),
+                info: resolve(__dirname, 'src/info.html'),
+                rehearsals: resolve(__dirname, 'src/rehearsals.html'),
+            }
+        }
     },
     server: {
         port: 8080
